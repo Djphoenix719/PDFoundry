7ba5284d7d98b14da6c3c8d685412a0e00165727
Andy <andrewcuccinello@gmail.com>
2020-07-02 01:16:31 -0400

    compile sass with gulp add sizes to gulp console move module source to src move css to src move templates to src

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 'use strict';
+const colors = require('colors');
+const fs = require('fs');
 
 const watchify = require('watchify');
 const browserify = require('browserify');
 const tsify = require('tsify');
 const babelify = require('babelify');
-const colors = require('colors');
+
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
 const gulp = require('gulp');
 const source = require('vinyl-source-stream');
@@ -16,12 +20,57 @@ const assign = require('lodash.assign');
 
 // add custom browserify options here
 const customOpts = {
-    entries: ['./module/main.ts'],
+    entries: ['./src/module/main.ts'],
     sourceType: 'module',
     debug: true,
 };
 const opts = assign({}, watchify.args, customOpts);
 const b = watchify(browserify(opts));
+
+function size(path) {
+    const sizes = [
+        ['B' , false, 1    ],
+        ['KB', true,  2**10],
+        ['MB', true,  2**20],
+        ['GB', true,  2**30],
+    ].reverse();
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    function format(n, decimals) {
+        if (decimals) {
+            return numberWithCommas(n.toFixed(2));
+        } else {
+            return numberWithCommas(n);
+        }
+    }
+
+    let bytes = _sizeRecursive(path);
+    if (bytes === 0) bytes = 1;
+    for (const [code, f, amount] of sizes) {
+        if (bytes > amount) {
+           return `${format(bytes/amount, f).magenta} ${code}`;
+        }
+    }
+    return `${format(bytes, false).magenta} ${'B'}`;
+}
+function _sizeRecursive(path) {
+    let s = 0;
+    const stats = fs.statSync(path);
+    if (stats.isDirectory()) {
+        const children = fs.readdirSync(path);
+        for (const child of children) {
+            s = s + _sizeRecursive(`${path}/${child}`)
+        }
+    } else {
+        s = stats.size;
+    }
+    return s;
+}
+
+const dest = './pdfoundry-dist/';
 
 // add transformations here
 // i.e. b.transform(coffeeify);
@@ -32,18 +81,30 @@ b.on('log', logger.info); // output build logs to terminal
 b.on('update', bundle);
 
 gulp.task('build', async () => {
-    gulp.watch("templates/**/*.html").on('change', copy_html);
-    gulp.watch("locale/**/*.json").on('change', copy_locale);
+    gulp.watch("templates/**/*.html").on('change', () => copy_dir('templates'));
+    gulp.watch("locale/**/*.json").on('change', () => copy_dir('locale', false));
+    gulp.watch("assets/**/*").on('change', () => copy_dir('assets', false));
+    gulp.watch("css/**/*.scss").on('change', () => gulp.start('sass'));
 
-    await copy_html();
-    await copy_locale();
+    await copy_dir('templates');
+    await copy_dir('locale', false);
+    await copy_dir('assets', false);
     await bundle();
+});
+
+gulp.task('sass', async() => {
+    gulp.src('./src/css/bundle.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dest));
 });
 
 async function bundle() {
     return b.bundle()
         // log errors if they happen
-        .on('error', logger.error.bind(logger, 'Browserify Error'))
+        .on('error', logger.error.bind(logger, 'Browserify Error'.red))
+        .on('end', () => log(`Bundle complete (${size(dest).magenta})`))
         .pipe(source('bundle.js'))
         // optional, remove if you don't need to buffer file contents
         .pipe(buffer())
@@ -51,23 +112,34 @@ async function bundle() {
         .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
         // Add transformation tasks to the pipeline here.
         .pipe(sourcemaps.write('./')) // writes .map file
-        .pipe(gulp.dest('./pdfoundry-dist'));
+        .pipe(gulp.dest(dest));
+}
+
+// https://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
 function log(message) {
     const time = new Date();
-    const timestamp = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`.gray;
+
+    const hh = pad(time.getHours(), 2);
+    const mm = pad(time.getMinutes(), 2);
+    const ss = pad(time.getSeconds(), 2);
+
+    const timestamp = `${hh}:${mm}:${ss}`.gray;
     console.log(`[${timestamp}] ${message}`);
 }
 
-async function copy_html() {
-    log('Copying HTML')
-    await del('./pdfoundry-dist/templates', {force: true});
-    await gulp.src(['./templates/**/*']).pipe(gulp.dest('./pdfoundry-dist/templates'));
-}
+async function copy_dir(name, src = true) {
+    let path = name;
+    if (src) {
+        path = `./src/${name}`
+    }
 
-async function copy_locale() {
-    log('Copying Locales')
-    await del('./pdfoundry-dist/locale', {force: true});
-    await gulp.src(['./locale/**/*']).pipe(gulp.dest('./pdfoundry-dist/locale'));
+    log(`Copying ${name.blue} (${size(path)})`);
+    await del(`${dest}/${name}`, {force: true});
+    await gulp.src([`${path}/**/*`]).pipe(gulp.dest(`./${dest}/${name}`));
 }
