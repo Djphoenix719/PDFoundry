933e5ce035f45938821d14fae4d548b09c9f0845
Andy <andrewcuccinello@gmail.com>
2020-07-10 15:49:13 -0400

    Begin refactoring gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -14,94 +14,185 @@
  */
 
 'use strict';
-const colors = require('colors');
 const fs = require('fs');
+const path = require('path');
+const del = require('del');
+const assign = require('lodash.assign');
+const chalk = require('chalk');
+const { exec, execFile } = require('child_process');
 
-const watchify = require('watchify');
+// Browserify
 const browserify = require('browserify');
+const watchify = require('watchify');
 const tsify = require('tsify');
 const babelify = require('babelify');
 
+// Sass
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
+// Gulp
 const gulp = require('gulp');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
-const del = require('del');
 const logger = require('gulplog');
 const sourcemaps = require('gulp-sourcemaps');
-const assign = require('lodash.assign');
 const typedoc = require('gulp-typedoc');
+const size = require('get-folder-size');
 
-// add custom browserify options here
-const customOpts = {
+// Config
+const distName = 'pdfoundry-dist';
+const destFolder = path.resolve(process.cwd(), distName);
+const destBundle = path.resolve(process.cwd(), 'bundle.js');
+const destCSS = path.resolve(process.cwd(), 'bundle.css');
+
+const baseArgs = {
     entries: ['./src/module/main.ts'],
     sourceType: 'module',
     debug: true,
 };
-const opts = assign({}, watchify.args, customOpts);
-const b = watchify(browserify(opts));
 
-function size(path) {
-    const sizes = [
-        ['B' , false, 1    ],
-        ['KB', true,  2**10],
-        ['MB', true,  2**20],
-        ['GB', true,  2**30],
-    ].reverse();
+// Watchify setup
+const watchArgs = assign({}, watchify.args, baseArgs);
+const watcher = watchify(browserify(watchArgs));
+watcher.plugin(tsify);
+watcher.transform(babelify);
+watcher.on('log', logger.info);
+watcher.on('update', bundle);
 
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-
-    function format(n, decimals) {
-        if (decimals) {
-            return numberWithCommas(n.toFixed(2));
-        } else {
-            return numberWithCommas(n);
-        }
-    }
-
-    let bytes = _sizeRecursive(path);
-    if (bytes === 0) bytes = 1;
-    for (const [code, f, amount] of sizes) {
-        if (bytes > amount) {
-           return `${format(bytes/amount, f).magenta} ${code}`;
-        }
-    }
-    return `${format(bytes, false).magenta} ${'B'}`;
-}
-function _sizeRecursive(path) {
-    let s = 0;
-    const stats = fs.statSync(path);
-    if (stats.isDirectory()) {
-        const children = fs.readdirSync(path);
-        for (const child of children) {
-            s = s + _sizeRecursive(`${path}/${child}`)
-        }
-    } else {
-        s = stats.size;
-    }
-    return s;
+/**
+ * UTILITIES
+ */
+/**
+ * Get size of folder
+ * @param path
+ * @return {Promise<Number>}
+ */
+async function sizeOf(path) {
+    return new Promise((resolve, reject) => {
+        size(path, function (err, size) {
+            if (err) reject(err);
+            else resolve(size);
+        });
+    });
 }
 
-const dest = './pdfoundry-dist/';
+/**
+ * Sleep for ms milliseconds then return
+ * @param ms
+ * @return {Promise<void>}
+ */
+async function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 
-// add transformations here
-// i.e. b.transform(coffeeify);
-b.plugin(tsify);
-b.transform(babelify);
+function getBabelConfig() {
+    return JSON.parse(fs.readFileSync('.babelrc').toString());
+}
 
-b.on('log', logger.info); // output build logs to terminal
-b.on('update', bundle);
+/**
+ * LINK
+ */
+gulp.task('link', async function () {
+    if (process.argv.length < 5) {
+        logger.error(`${chalk.red('Link requires 2 arguments')}: gulp link --system <system name>`);
+        return;
+    }
+    const [nodePath, gulpPath, task, linkType, linkName] = process.argv;
 
+    if (linkType !== '--system') {
+        logger.error(`${chalk.red('Error: Curently only --system is supported')}`);
+        return;
+    }
+
+    const systemPath = path.resolve(process.cwd(), '../..', 'systems', linkName);
+    const targetPath = path.resolve(systemPath, distName);
+
+    logger.info(`Linking to "${chalk.green(targetPath)}"`);
+
+    try {
+        if (fs.readlinkSync(targetPath)) {
+            fs.unlinkSync(targetPath);
+        }
+    } catch (error) {
+        // Link does not exist
+    }
+
+    // For some reason fs.symlink perm errors, so we'll execute mklink
+    // Additional support would be needed for linux based systems here
+    exec(`mklink /J ${targetPath} ${destFolder}`, function (error, stdout, stderr) {
+        if (error) logger.error(chalk.red(error));
+        else logger.info(chalk.green(`Symlink created.`));
+
+        process.chdir(path.resolve(targetPath, '..'));
+
+        // Run install script to setup that system
+        require(path.resolve(process.cwd(), distName, 'scripts', 'install.js'));
+    });
+});
+
+/**
+ * CLEAN
+ */
+gulp.task('clean', async function () {
+    const files = fs.readdirSync(destFolder);
+    for (const file of files) {
+        await del(path.resolve(destFolder, file));
+    }
+});
+
+/**
+ * REBUILD
+ */
+gulp.task('rebuild', async () => {
+    return gulp.series(['clean', 'build']);
+});
+
+/**
+ * BUILD
+ */
 gulp.task('build', async () => {
-    gulp.watch("src/templates/**/*.html").on('change', () => copy_dir('templates', true));
-    gulp.watch("locale/**/*.json").on('change', () => copy_dir('locale'));
-    gulp.watch("assets/**/*").on('change', () => copy_dir('assets'));
-    gulp.watch("src/css/**/*.scss").on('change', () => build_sass());
-    gulp.watch("scripts/**/*.js").on('change', () => copy_dir('scripts'));
+    await copyStatic();
+
+    const babel = babelify.configure(getBabelConfig());
+
+    return browserify({
+        entries: ['src/module/main.ts'],
+        transform: babel,
+        plugin: tsify,
+        debug: true,
+    })
+        .bundle()
+        .on('log', logger.info)
+        .on('error', logger.error)
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(destFolder));
+});
+
+async function copyStatic() {
+    gulp.src('assets/**/*').pipe(gulp.dest(path.resolve(destFolder, 'assets')));
+    gulp.src('manual/**/*.pdf').pipe(gulp.dest(path.resolve(destFolder, 'assets', 'manual')));
+    gulp.src('src/templates/**/*').pipe(gulp.dest(path.resolve(destFolder, 'templates')));
+    gulp.src('locale/**/*').pipe(gulp.dest(path.resolve(destFolder, 'locale')));
+    gulp.src('pdfjs/**/*').pipe(gulp.dest(path.resolve(destFolder, 'pdfjs')));
+    gulp.src('src/scripts/**/*').pipe(gulp.dest(path.resolve(destFolder, 'scripts')));
+    gulp.src('LICENSE').pipe(gulp.dest(destFolder));
+}
+
+/**
+ * WATCH
+ */
+gulp.task('watch', async () => {
+    gulp.watch('src/templates/**/*.html').on('change', () => copy_dir('templates', true));
+    gulp.watch('locale/**/*.json').on('change', () => copy_dir('locale'));
+    gulp.watch('assets/**/*').on('change', () => copy_dir('assets'));
+    gulp.watch('src/css/**/*.scss').on('change', () => build_sass());
+    gulp.watch('scripts/**/*.js').on('change', () => copy_dir('scripts'));
 
     await copy_dir('templates', true);
     await copy_dir('locale');
@@ -110,17 +201,17 @@ gulp.task('build', async () => {
     await copy_dir('scripts');
     await copy_file('LICENSE');
 
-    gulp.src("manual/**/*.pdf").pipe(gulp.dest(`${dest}assets/manual/`));
+    gulp.src('manual/**/*.pdf').pipe(gulp.dest(`${dest}assets/manual/`));
 
     await build_sass();
     await bundle();
 });
 
-gulp.task('sass', async() => {
+gulp.task('sass', async () => {
     await build_sass();
 });
 
-gulp.task('docs', function() {
+gulp.task('docs', function () {
     return gulp
         .src([
             'src/module/api/**/*',
@@ -130,80 +221,68 @@ gulp.task('docs', function() {
             'src/module/types/**/*',
             'src/module/viewer/**/*',
         ])
-        .pipe(typedoc({
-            name: "PDFoundry",
-            target: "es6",
-            out: "docs/",
-            mode: 'file',
-            excludePrivate: true,
-            excludeProtected: true,
-            version: true,
-            plugins: [
-                'typedoc-plugin-external-module-name'
-            ]
-        }));
+        .pipe(
+            typedoc({
+                name: 'PDFoundry',
+                target: 'es6',
+                out: 'docs/',
+                mode: 'file',
+                excludePrivate: true,
+                excludeProtected: true,
+                version: true,
+                plugins: ['typedoc-plugin-external-module-name'],
+            }),
+        );
 });
-
 
 async function build_sass() {
     gulp.src('./src/css/bundle.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(dest));
 }
 
 async function bundle() {
-    return b.bundle()
-        // log errors if they happen
-        .on('error', logger.error.bind(logger, 'Browserify Error'.red))
-        .on('end', () => log(`Bundle complete (${size(dest).magenta})`))
-        .pipe(source('bundle.js'))
-        // optional, remove if you don't need to buffer file contents
-        .pipe(buffer())
-        // optional, remove if you dont want sourcemaps
-        .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-        // Add transformation tasks to the pipeline here.
-        .pipe(sourcemaps.write('./')) // writes .map file
-        .pipe(gulp.dest(dest));
-}
-
-// https://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript
-function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
-
-function log(message) {
-    const time = new Date();
-
-    const hh = pad(time.getHours(), 2);
-    const mm = pad(time.getMinutes(), 2);
-    const ss = pad(time.getSeconds(), 2);
-
-    const timestamp = `${hh}:${mm}:${ss}`.gray;
-    console.log(`[${timestamp}] ${message}`);
+    return (
+        watcher
+            .bundle()
+            // log errors if they happen
+            .on('error', logger.error.bind(logger, 'Browserify Error'.red))
+            .on('end', () => log(`Bundle complete (${size(dest).magenta})`))
+            .pipe(source('bundle.js'))
+            // optional, remove if you don't need to buffer file contents
+            .pipe(buffer())
+            // optional, remove if you dont want sourcemaps
+            .pipe(sourcemaps.init({ loadMaps: true })) // loads map from browserify file
+            // Add transformation tasks to the pipeline here.
+            .pipe(sourcemaps.write('./')) // writes .map file
+            .pipe(gulp.dest(dest))
+    );
 }
 
 async function copy_dir(name, src = false) {
     let path = name;
     if (src) {
-        path = `./src/${name}`
+        path = `./src/${name}`;
     }
 
-    log(`Copying ${name.blue} (${size(path)})`);
-    await del(`${dest}/${name}`, {force: true});
-    await gulp.src([`${path}/**/*`]).pipe(gulp.dest(`./${dest}/${name}`));
+    await del(`${destFolder}/${name}`, { force: true });
+    await gulp
+        .src([`${path}/**/*`])
+        .pipe(size())
+        .pipe(gulp.dest(`./${destFolder}/${name}`));
 }
 
 async function copy_file(name, src = false) {
     let path = name;
     if (src) {
-        path = `./src/${name}`
+        path = `./src/${name}`;
     }
 
-    log(`Copying ${name.blue} (${size(path)})`);
-    await del(`${dest}/${name}`, {force: true});
-    await gulp.src([`${path}`]).pipe(gulp.dest(`./${dest}`));
+    await del(`${destFolder}/${name}`, { force: true });
+    await gulp
+        .src([`${path}`])
+        .pipe(size())
+        .pipe(gulp.dest(`./${destFolder}`));
 }
