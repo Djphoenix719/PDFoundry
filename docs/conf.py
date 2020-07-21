4adf038022207ff6cbccad150ebcf9273053fc2d
Andy <andrewcuccinello@gmail.com>
2020-07-21 12:24:26 -0400

    Test readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-run('pip install sphinx-js')
-
 # -- Project information -----------------------------------------------------
 
 project = 'PDFoundry'
