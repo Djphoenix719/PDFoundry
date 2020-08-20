00b54c45e58f3635f77e7966f8170c463f0937e0
Andy <andrewcuccinello@gmail.com>
2020-08-19 21:11:51 -0400

    White-list "assets-forgevtt.com"

--- src/pdfoundry/common/Whitelist.ts
+++ src/pdfoundry/common/Whitelist.ts
@@ -17,4 +17,4 @@
  * The domain white list includes domains that are allowed other than 'localhost' or
  *  the equivalent domain the user is running the server on.
  */
-export const DOMAIN_WHITELIST = ['amazonaws.com', 'digitaloceanspaces.com'];
+export const DOMAIN_WHITELIST = ['amazonaws.com', 'digitaloceanspaces.com', 'assets-forgevtt.com'];
